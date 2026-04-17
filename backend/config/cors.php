38287e83c53c06e894196212de0b13a56042a854
Jeperlyn <jeflynjoy.solito@tup.edu.ph>
2026-04-17 22:05:45 +0800

    Change allowed_origins to allow all origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -13,14 +13,7 @@ return [
     'allowed_methods' => ['*'],
 
     // We changed this to '*' to allow your friend's IP to talk to your database
-'allowed_origins' => [
-    'http://localhost:5173', 
-    'https://cityjoblink-frontend.vercel.app', // Your Vercel frontend
-    'https://cityjoblink-frontend-git-main-jeperlyns-projects.vercel.app',
-    'https://cityjoblink-frontend.vercel.app',
-    'https://cityjoblink-frontend-g4t7rtauq-jeperlyns-projects.vercel.app',
-    'https://choosy-showroom-baking.ngrok-free.dev', // Your new public API
-],
+'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
